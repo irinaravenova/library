cd786597c3e2aa2fdd3f2b5cad4c57acb8a545bd
irinaravenova <hernandezirina23@gmail.com>
2023-09-23 11:35:52 -0400

    color and styling added

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+let title = document.getElementById('title');
+let author = document.getElementById('author');
+let pages = document.getElementById('pages');
+let read = document.getElementById('read');
+
 const showButton = document.getElementById("showDialog");
 const favDialog = document.getElementById("favDialog");
 const outputBox = document.querySelector("output");
@@ -19,23 +24,26 @@ confirmBtn.addEventListener("click", (event) => {
   });
 
 const theHobbit = new Book("The Hobbit", "JRR Tolkien", "25", "not read yet")
-const lotr = new Book("The Hobbit", "JRR Tolkien", "25", "not read yet")
-const lotr1 = new Book("The Hobbit", "JRR Tolkien", "25", "not read yet")
-const lotr2 = new Book("The Hobbit", "JRR Tolkien", "25", "not read yet")
+const lotr = new Book("LOTR", "JRR Tolkien", "25", "not read yet")
+const lotr1 = new Book("LOTR 1", "JRR Tolkien", "25", "not read yet")
+const lotr2 = new Book("LOTR 2", "JRR Tolkien", "25", "not read yet")
 
 const myLibrary = [lotr1, theHobbit, lotr, lotr2]
 console.log(myLibrary)
 
-let title = document.getElementById('title')
-let author = document.getElementById('author')
-let pages = document.getElementById('pages')
-let read = document.getElementById('read')
+
 
 myLibrary.forEach((book) => {
     const booksList = document.getElementById("booksList");
     let listedBook = document.createElement('li');
     let deleteEntryBtn = document.createElement('button');
-    listedBook.innerHTML = `${book.title}, ${book.author}, ${book.pages} pages, ${book.read}`;
+    deleteEntryBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>';
+
+    listedBook.innerHTML = `<p>${book.title}</p> 
+                            <p>${book.author}</p> 
+                            <p>${book.pages} pages</p> 
+                            <p>${book.read}</p>`;
+
     booksList.appendChild(listedBook);
     listedBook.appendChild(deleteEntryBtn);
 })
@@ -53,19 +61,21 @@ document.getElementById('submit').addEventListener("click", function(event) {
     
     const existingBooks = document.getElementById("booksList");
     let newBook = document.createElement('li');
-    newBook.innerHTML = `${newBookObject.title}, ${newBookObject.author}, ${newBookObject.pages} pages, ${readOrNot}`;
+    newBook.innerHTML = `<p> ${newBookObject.title}</p> 
+                        <p>${newBookObject.author}</p> 
+                        <p>${newBookObject.pages} pages</p> 
+                        <p>${readOrNot}</p>`;
     
     existingBooks.appendChild(newBook);
     favDialog.close();
     resetBookForm();
-    console.log(myLibrary)
+    console.log(myLibrary);
 
 })
 
 function resetBookForm() {
     document.getElementById('addBookForm').reset();
 }
-
 
 function Book(title, author, pages, read) {
 
@@ -83,7 +93,7 @@ function addBookToLibrary(title, author, pages, read) {
     
     const newBookObject = new Book(title.value, author.value, pages.value, read.checked);
     myLibrary.push(newBookObject);
-    console.log(newBookObject)
+    console.log(newBookObject);
     return newBookObject;
   }
 });
