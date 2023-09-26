c7db3d80f39de1481f26ec1bc67061ad8b469765
irinaravenova <hernandezirina23@gmail.com>
2023-09-25 23:38:18 -0400

    now can add and delete book objects from array and DOM

--- script.js
+++ script.js
@@ -14,84 +14,75 @@ const showButton = document.getElementById("showDialog");
 const favDialog = document.getElementById("favDialog");
 const outputBox = document.querySelector("output");
 const confirmBtn = favDialog.querySelector(".confirmBtn");
+const ul = document.querySelector("ul");
 
-const myLibrary = [lotr];
+const myLibrary = [lotr, lotr1, lotr2];
 emptyCheck();
 console.log(myLibrary)
 
 
-// Displays empty library message if no books in myLibrary array.
+// TODO - Display empty library message if no books in myLibrary array.
 function emptyCheck() {
     if (myLibrary.length === 0) { 
-        let a123 = document.getElementsByClassName('library-empty-msg');
-        a123[0].innerText = "Your library is empty!"
-        console.log(a123[0].innerText)
+        // TODO If myLibrary is empty
      }
-     else if (myLibrary.length == 1) {
-        let a123 = document.getElementsByClassName('library-empty-msg');
-        a123[0].remove();
-        return;
+    else if (myLibrary.length > 0) {
+        // TODO
     }
     else {
         return;
     }
 }
 
-for (let book = 0; book < myLibrary.length; book++) {
+
+// Loop over myLibrary array to display book ojects on individual cards.
+for (let bookIndex = 0; bookIndex < myLibrary.length; bookIndex++) {
     const booksList = document.getElementById("booksList");
     let listedBook = document.createElement('li');
     let deleteEntryBtn = document.createElement('button');
     deleteEntryBtn.className = "delete-entry-button";
     let readStatus = "";
-    if (book.read == false) {
+    if (bookIndex.read == false) {
         readStatus = "Not Completed";
     }
     else {
         readStatus = "Completed";
     }
-    listedBook.innerHTML = `<p>${book.title}</p> 
-                            <p>${book.author}</p> 
-                            <p>${book.pages} pages</p> 
+    listedBook.innerHTML = `<p class="title">${myLibrary[bookIndex].title}</p> 
+                            <p>${myLibrary[bookIndex].author}</p> 
+                            <p>${myLibrary[bookIndex].pages} pages</p> 
                             <p>${readStatus}</p>`;
     deleteEntryBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>';
     
-
-    listedBook.dataset.id=`${book}`;
+    listedBook.dataset.id=`${bookIndex}`;
     console.log(listedBook.dataset.id);
-    
+
+    // eventListener on delete button to remove book from myLibrary and delete element from HTML.
+    listedBook.addEventListener('click', function (e) {
+        if (e.target.parentElement.tagName == "BUTTON" || e.target.parentElement.tagName == "svg") {
+                let bookTitle = listedBook.querySelector('.title').innerHTML;
+
+                // Nested loop to look for matching book title in myLibrary array.
+                for (let book in myLibrary) {
+                    for (title in book) {
+                        if(myLibrary[book].title == bookTitle) {
+                            console.log(`${myLibrary[book].title} & ${bookTitle} match!`)
+                            console.log(myLibrary.indexOf((myLibrary[book])))
+                            myLibrary.splice((myLibrary.indexOf((myLibrary[book]))), 1)
+                            listedBook.remove();
+                            console.log(myLibrary)
+                        }
+                    }
+                }
+        }
+        }
+    )
     booksList.appendChild(listedBook);
     listedBook.appendChild(deleteEntryBtn);
 }
 
-// Displays each book in myLibrary array.
-// myLibrary.forEach((book) => {
-//     const booksList = document.getElementById("booksList");
-//     let listedBook = document.createElement('li');
-//     let deleteEntryBtn = document.createElement('button');
-//     deleteEntryBtn.className = "delete-entry-button";
-//     let readStatus = "";
-//     if (book.read == false) {
-//         readStatus = "Not Completed";
-//     }
-//     else {
-//         readStatus = "Completed";
-//     }
-//     listedBook.innerHTML = `<p>${book.title}</p> 
-//                             <p>${book.author}</p> 
-//                             <p>${book.pages} pages</p> 
-//                             <p>${readStatus}</p>`;
-//     deleteEntryBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>';
-    
 
-//     listedBook.dataset.id=`${Number(myLibrary[0])}`;
-//     console.log(listedBook.dataset.id);
-    
-//     booksList.appendChild(listedBook);
-//     listedBook.appendChild(deleteEntryBtn);
-// })
-
-
-// Button event listeners.
+// Add new book modal event listeners.
 showButton.addEventListener("click", () => {
     favDialog.showModal();
   });
@@ -101,13 +92,12 @@ favDialog.addEventListener("close", (e) => {
 });
 
 confirmBtn.addEventListener("click", (event) => {
-    event.preventDefault() // We don't want to submit this fake form
-    favDialog.close(); // Have to send the select box value here.
+    event.preventDefault() 
+    favDialog.close();
   });
 
 
-
-// Adds new book function to myLibrary array.
+// Adds new book function to myLibrary array on submit from Add New Book modal.
 document.getElementById('submit').addEventListener("click", function(event) {
     event.preventDefault(); 
     let readOrNot;
@@ -122,21 +112,42 @@ document.getElementById('submit').addEventListener("click", function(event) {
     let newBook = document.createElement('li');
     let deleteEntryBtn = document.createElement('button');
     deleteEntryBtn.className = "delete-entry-button";
-    newBook.innerHTML = `<p> ${newBookObject.title}</p> 
+    newBook.dataset.id=`${myLibrary.length-1}`;
+    newBook.innerHTML = `<p class="title1">${newBookObject.title}</p> 
                         <p>${newBookObject.author}</p> 
                         <p>${newBookObject.pages} pages</p> 
                         <p>${readOrNot}</p>`;
 
     deleteEntryBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>';    
+    
+    // Adds new book as a card to the main container.
     existingBooks.appendChild(newBook);
     newBook.appendChild(deleteEntryBtn);
     favDialog.close();
     resetBookForm();
-
-    newBook.dataset.id=`${myLibrary.length-1}`;
     console.log(newBook.dataset.id);
+    console.log(myLibrary)
 
-    emptyCheck();
+    //emptyCheck();
+
+    // Event listener put on button which deletes book from myLibrary && from HTML.
+    newBook.addEventListener('click', function (e) {
+        if (e.target.parentElement.tagName == "BUTTON" || e.target.parentElement.tagName == "svg") {
+            let bookTitle = newBook.querySelector('.title1').innerHTML;
+            for (let book in myLibrary) {
+                for (title in book) {
+                    if(myLibrary[book].title == bookTitle) {
+                        console.log(`${myLibrary[book].title} & ${bookTitle} match!`)
+                        console.log(myLibrary.indexOf((myLibrary[book])))
+                        myLibrary.splice((myLibrary.indexOf((myLibrary[book]))), 1)
+                        newBook.remove();
+                        console.log(myLibrary)
+                    }
+                }
+            }
+        }
+    }
+    )
 })
 
 
@@ -146,7 +157,7 @@ function resetBookForm() {
 }
 
 
-// The Book object prototype constructor function.
+// The Book object prototype // constructor function.
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -158,23 +169,15 @@ function Book(title, author, pages, read) {
 }
 
 
-// Function that creates new book referencing the prototype.
+// Function that creates new book using the Book prototype above.
 function addBookToLibrary(title, author, pages, read) {
-    const newBookObject = new Book(title.value, author.value, pages.value, read.checked);
-    myLibrary.push(newBookObject);
+    let title1 = document.getElementById('title');
+    const newBookObject = new Book(title1.value, author.value, pages.value, read.checked);
     console.log(newBookObject);
+    myLibrary.push(newBookObject);
+    emptyCheck();
     return newBookObject;
   }
-
-
-// window.onload = function() {
-// const deleteBtn = document.getElementsByClassName(".delete-entry-button");
-// deleteBtn.addEventListener("click", (event) => {
-//     console.log(event);
-// });
-    
-//   }
-
 
 
 });
