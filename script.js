c324a4d50e2dda533dc701de498ace2db29eccff
irinaravenova <hernandezirina23@gmail.com>
2023-09-28 11:05:15 -0400

    toggle button added to elements

--- script.js
+++ script.js
@@ -39,27 +39,33 @@ function emptyCheck() {
 for (let bookIndex = 0; bookIndex < myLibrary.length; bookIndex++) {
     const booksList = document.getElementById("booksList");
     let listedBook = document.createElement('li');
+
+    let toggleButton = document.createElement('button');
+    toggleButton.className = "toggle-button";
+
     let deleteEntryBtn = document.createElement('button');
     deleteEntryBtn.className = "delete-entry-button";
-    let readStatus = "";
-    if (bookIndex.read == false) {
-        readStatus = "Not Completed";
+
+    let readStatus;
+
+    if (myLibrary[bookIndex].read == false) {
+        readStatus = "In Progress";
     }
-    else {
-        readStatus = "Completed";
+    else if (myLibrary[bookIndex].read == true) {
+        readStatus = "Book Complete";
     }
+
     listedBook.innerHTML = `<p class="title">${myLibrary[bookIndex].title}</p> 
                             <p>${myLibrary[bookIndex].author}</p> 
-                            <p>${myLibrary[bookIndex].pages} pages</p> 
-                            <p>${readStatus}</p>`;
+                            <p>${myLibrary[bookIndex].pages} pages</p>`;
     deleteEntryBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>';
-    
+    toggleButton.innerHTML = `${readStatus}`
+    listedBook.appendChild(toggleButton);
     listedBook.dataset.id=`${bookIndex}`;
-    console.log(listedBook.dataset.id);
 
     // eventListener on delete button to remove book from myLibrary and delete element from HTML.
     listedBook.addEventListener('click', function (e) {
-        if (e.target.parentElement.tagName == "BUTTON" || e.target.parentElement.tagName == "svg") {
+        if (e.target.parentElement.className == "delete-entry-button" || e.target.parentElement.tagName == "svg") {
                 let bookTitle = listedBook.querySelector('.title').innerHTML;
 
                 // Nested loop to look for matching book title in myLibrary array.
@@ -77,8 +83,44 @@ for (let bookIndex = 0; bookIndex < myLibrary.length; bookIndex++) {
         }
         }
     )
+
+    toggleButton.addEventListener('click', function(e) {
+        let bookTitle = listedBook.querySelector('.title').innerHTML;
+
+        // toggleButton.style.backgroundColor === "orange" ? "green" : "orange";
+
+        for (let book in myLibrary) {
+            for (title in book) {
+
+                if(myLibrary[book].title == bookTitle) {
+
+                let newReadStatus = (!(myLibrary[book].read));
+                myLibrary[book].read = newReadStatus;
+
+                if (myLibrary[book].read == false) {
+                    toggleButton.innerHTML = "In Progress"
+                    toggleButton.style.backgroundColor = "orange"
+
+                    
+                }
+                else {
+                    toggleButton.innerHTML = "Book Complete"
+                    toggleButton.style.backgroundColor = "green"
+
+                }
+
+                console.log(myLibrary[book])
+                return
+                }
+            }
+        }
+        
+
+    })
+
     booksList.appendChild(listedBook);
     listedBook.appendChild(deleteEntryBtn);
+    
 }
 
 
@@ -100,39 +142,47 @@ confirmBtn.addEventListener("click", (event) => {
 // Adds new book function to myLibrary array on submit from Add New Book modal.
 document.getElementById('submit').addEventListener("click", function(event) {
     event.preventDefault(); 
+    console.log(title.value)
     let readOrNot;
     let newBookObject = addBookToLibrary(title, author, pages, read);
     if (newBookObject.read == true) {
-        readOrNot = "Completed";
+        readOrNot = "Book Complete";
     }
     else {
-        readOrNot = "Not Completed";
+        readOrNot = "In Progress";
     }
     const existingBooks = document.getElementById("booksList");
     let newBook = document.createElement('li');
+
     let deleteEntryBtn = document.createElement('button');
     deleteEntryBtn.className = "delete-entry-button";
+
+    let toggleButton = document.createElement('button');
+    toggleButton.className = "toggle-button";
+
     newBook.dataset.id=`${myLibrary.length-1}`;
     newBook.innerHTML = `<p class="title1">${newBookObject.title}</p> 
                         <p>${newBookObject.author}</p> 
-                        <p>${newBookObject.pages} pages</p> 
-                        <p>${readOrNot}</p>`;
+                        <p>${newBookObject.pages} pages</p>`;
 
     deleteEntryBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>';    
-    
+    toggleButton.innerHTML = `${readOrNot}`
+
     // Adds new book as a card to the main container.
     existingBooks.appendChild(newBook);
+    newBook.appendChild(toggleButton);
     newBook.appendChild(deleteEntryBtn);
+    
     favDialog.close();
     resetBookForm();
-    console.log(newBook.dataset.id);
     console.log(myLibrary)
+    console.log(newBookObject.read)
 
     //emptyCheck();
 
     // Event listener put on button which deletes book from myLibrary && from HTML.
     newBook.addEventListener('click', function (e) {
-        if (e.target.parentElement.tagName == "BUTTON" || e.target.parentElement.tagName == "svg") {
+        if (e.target.parentElement.className == "delete-entry-button" || e.target.parentElement.tagName == "svg") {
             let bookTitle = newBook.querySelector('.title1').innerHTML;
             for (let book in myLibrary) {
                 for (title in book) {
@@ -148,6 +198,33 @@ document.getElementById('submit').addEventListener("click", function(event) {
         }
     }
     )
+
+    // Toggle if book is read or not
+    toggleButton.addEventListener('click', function(e) {
+        let bookTitle = newBook.querySelector('.title1').innerHTML;
+        for (let book in myLibrary) {
+                for (title in book) {
+                    if(myLibrary[book].title == bookTitle) {
+                    let newReadStatus = (!(myLibrary[book].read));
+                    myLibrary[book].read = newReadStatus;
+                    console.log(myLibrary[book])
+                    if (myLibrary[book].read == false) {
+                        toggleButton.innerHTML = "In Progress"
+                        toggleButton.style.backgroundColor = "orange"
+
+                    }
+                    else {
+                        toggleButton.innerHTML = "Book Complete"
+                        toggleButton.style.backgroundColor = "green"
+
+                    }
+                    return
+                }
+                }
+            }
+        }
+    )
+
 })
 
 
